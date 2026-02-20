4a2da9501822df74070cb83b788b1f4ecae0262f
Emily Monterrosa <emilymonterrosa.c@gmail.com>
2026-02-19 22:33:24 -0500

    Limpiar

--- atlas-bank/src/main/java/com/bank/atlas_bank/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bank.atlas_bank.repository;
-
-import com.bank.atlas_bank.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- atlas-bank/src/main/java/com/bank/atlas_bank/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bank.atlas_bank.repository;
-
-import com.bank.atlas_bank.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
